a47095b2bd7bcbfa54b40cc277398670ef25a37a
Evgenii Gusev <ged1994@mail.ru>
2023-12-12 19:33:12 +0300

    added logging settings

--- wishlist/project/settings/templates/settings.dev.py
+++ wishlist/project/settings/templates/settings.dev.py
@@ -1,2 +1,10 @@
 DEBUG = True
 SECRET_KEY = 'django-insecure-u!_(pjmv$nu0(u!w1s(w48xtoy&9*xz(i!1i3hhn26eqa+$mgy'
+
+LOGGING['formatters']['colored'] = {  # type: ignore
+    '()': 'colorlog.ColoredFormatter',
+    'format': '%(log_color)s%(asctime)s %(levelname)s %(name)s %(bold_white)s%(message)s',
+}
+LOGGING['loggers']['wishlist']['level'] = 'DEBUG'  # type: ignore
+LOGGING['handlers']['console']['level'] = 'DEBUG'  # type: ignore
+LOGGING['handlers']['console']['formatter'] = 'colored'  # type: ignore
