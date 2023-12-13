1607b24d123ce81baa1ab8a27614d3e985ae274c
Evgenii Gusev <ged1994@mail.ru>
2023-12-13 13:56:36 +0300

    added whitenoise for serving static

--- wishlist/project/settings/docker.py
+++ wishlist/project/settings/docker.py
@@ -2,3 +2,13 @@ if IN_DOCKER:  # type: ignore # noqa: F821
     assert MIDDLEWARE[:1] == [  # type: ignore # noqa: F821
         'django.middleware.security.SecurityMiddleware'
     ]
+    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')  # type: ignore # noqa: F821
+
+    STORAGES = {
+        'default': {
+            'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage'
+        },
+        'staticfiles': {
+            'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+        }
+    }
